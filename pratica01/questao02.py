9f95a26435d7f3485d797609137bbf9128b3f200
jonathantilio <jhotilio@gmail.com>
2026-01-08 20:29:14 -0300

    respostas completas

--- pratica01/questao02.py
+++ pratica01/questao02.py
@@ -1,4 +1,5 @@
-
+"""2- Calculadora de Soma
+* Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado."""
 
 
 
